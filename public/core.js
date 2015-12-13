3c97e78e1c15179e66cea6d9825e86b2723c4550
m-roche <mr474@bath.ac.uk>
2015-12-13 17:40:14 +0000

    AngularJS application and controller
    Functions to get all films, create a film, and delete a film.

--- public/core.js
+++ public/core.js
@@ -1,1 +1,44 @@
-//angular js code for app
+var filmList = angular.module('filmList', []);
+
+function mainController($scope, $http) {
+    $scope.formData = {};
+
+    // when landing on the page, get all films and show them
+    $scope.getFilms = function() {
+      $http.get('/api/films')
+          .success(function(data) {
+              $scope.films = data;
+              console.log(data);
+          })
+          .error(function(data) {
+              console.log('Error: ' + data);
+          });
+    };
+    $scope.getFilms();
+    // when submitting the add form, send the text to the node API
+    $scope.createFilm = function() {
+        $http.post('/api/films', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so the user is ready to enter another
+                //$scope.films = data;
+                console.log(data);
+                $scope.getFilms();
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // delete a film after checking it
+    $scope.deleteFilm = function(id) {
+        $http.delete('/api/films/' + id)
+            .success(function(data) {
+                $scope.getFilms();
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+}
